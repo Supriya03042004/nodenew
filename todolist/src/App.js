2d32990b0a08d4d4482d217ee38b7d9af73bb7bf
supriya03042004 <supriyakushwaha542@gmail.com>
2023-12-22 17:55:19 +0530

    first commit

---
+++ todolist/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+
+  return(
+  <div className="App">
+  <div className="search-Bar-container">
+    <div>searchBar</div>
+    <div >Search Results</div>
+  </div>
+
+
+</div>
+  )
+}
+
+export default App
+
+     
